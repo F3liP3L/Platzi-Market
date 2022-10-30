7721900dc3fedef64d8d3ce7e6b48ddf80295c41
Felipe Cardona <felipe7u7432@gmail.com>
2022-10-29 23:03:04 -0500

    Se conecto a la base de datos y se construyo el CRUD

--- src/main/java/com/platzi/market/HolaMundoController.java
+++ src/main/java/com/platzi/market/HolaMundoController.java
@@ -1,4 +1,4 @@
-package com.platzimarket;
+package com.platzi.market;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/com/platzi/market/persistence/entity/Categoria.java
@@ -0,0 +1,43 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Long id;
+    private String descripcion;
+    private Boolean estado;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
